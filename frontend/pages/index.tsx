15157040fd1e3ad028f0560c18500bf0718f2c1c
12siragi <siragisaidi218@gmail.com>
2025-02-08 03:05:06 +0300

    frontendnavbar

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+
+import Navbar from "../components/sections/navbar/default";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen w-full overflow-hidden bg-background text-foreground">
+      <Navbar />
+   
+    </main>
+  );
+}
