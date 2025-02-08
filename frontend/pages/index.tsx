c06554882f02baf4260f447f609d4ab9666dac01
12siragi <siragisaidi218@gmail.com>
2025-02-08 04:18:52 +0300

    adfriendfooter

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,22 @@
 
 import Navbar from "../components/sections/navbar/default";
+import Footer from "../components/ui/Footer";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full overflow-hidden bg-background text-foreground">
       <Navbar />
+
+
+
+
+
+
+
+
+
+      
+      <Footer />
    
     </main>
   );
