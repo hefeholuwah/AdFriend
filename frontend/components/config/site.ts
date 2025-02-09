5ebf02634b79f0e6af8dc4bbe3e8b8aa12fe80a4
Bruka25 <bkteshome862@gmail.com>
2025-02-09 17:56:21 +0300

    a manifest json file for chrome extension

--- frontend/components/config/site.ts
+++
@@ -1,7 +0,0 @@
-export const siteConfig = {
-    url: "https://yourwebsite.com",
-    links: {
-      github: "https://github.com/yourusername",
-    },
-  };
-  
--- frontend/components/ui/section.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import React from "react";
-
-export function Section({ children }: { children: React.ReactNode }) {
-  return <section className="px-6 py-12">{children}</section>;
-}
